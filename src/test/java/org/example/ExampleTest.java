8aee0291ed94c63355ca35ea021f1d4cee5c400f
Teona Todorovska <todorovska79@gmail.com>
2025-05-05 21:43:02 +0200

    Initial commit for Appium Reminder tests

---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Appium test goes here!");
+    }
+}
+
+
